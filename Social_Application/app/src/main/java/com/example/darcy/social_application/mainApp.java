05be554d01ff6873eeeb83066b4438094a2bb087
fwg7192 <fwg7192@autuni.ac.nz>
2017-09-21 20:35:15 +1200

    new code

---
+++ Social_Application/app/src/main/java/com/example/darcy/social_application/mainApp.java
@@ -0,0 +1,13 @@
+package com.example.darcy.social_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mainApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_app);
+    }
+}
